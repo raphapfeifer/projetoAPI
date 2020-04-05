ed4a99cc2e9311b1dca83d30f676a7e72e838ff9
raphapfeifer <rapha_pfeifer@yahoo.com.br>
2020-04-05 17:21:01 -0300

    Inclusão de DTO

--- forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,24 +1,23 @@
 package br.com.alura.forum.controller;
 
+import br.com.alura.forum.controller.dto.TopicoDto;
 import br.com.alura.forum.modelo.Curso;
 import br.com.alura.forum.modelo.Topico;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 import java.util.List;
 
-@Controller
+@RestController
 public class TopicosController {
 
     @RequestMapping("/topicos")
-    @ResponseBody
-    public List<Topico> lista(){
+    public List<TopicoDto> lista(){
 
-            Topico topico = new Topico("Duvida","Duvida com Spring",new Curso("Sprring","Programação"));
+            Topico topico = new Topico("Dúvida","Dúvida com Spring",new Curso("Sprring","Programação"));
 
-            return Arrays.asList(topico,topico,topico);
+            return TopicoDto.converter(Arrays.asList(topico,topico,topico));
     }
 
 }
