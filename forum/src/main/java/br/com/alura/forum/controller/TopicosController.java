e614db2e4962c03916839aa0970a43c7120e7299
raphapfeifer <rapha_pfeifer@yahoo.com.br>
2020-04-12 23:46:33 -0300

    Inclusão de repository

--- forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -3,6 +3,8 @@ package br.com.alura.forum.controller;
 import br.com.alura.forum.controller.dto.TopicoDto;
 import br.com.alura.forum.modelo.Curso;
 import br.com.alura.forum.modelo.Topico;
+import br.com.alura.forum.repository.TopicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,12 +14,13 @@ import java.util.List;
 @RestController
 public class TopicosController {
 
+    @Autowired
+    private TopicoRepository topicoRepository;
+
     @RequestMapping("/topicos")
     public List<TopicoDto> lista(){
-
-            Topico topico = new Topico("Dúvida","Dúvida com Spring",new Curso("Sprring","Programação"));
-
-            return TopicoDto.converter(Arrays.asList(topico,topico,topico));
+            List<Topico> topicos = topicoRepository.findAll();
+            return TopicoDto.converter(topicos);
     }
 
 }
