81586bf0cf416a764c67fc5f1efe7e9be088e663
raphapfeifer <rapha_pfeifer@yahoo.com.br>
2020-05-05 00:12:32 -0300

    Incluir método remover

--- forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -42,6 +42,7 @@ public class TopicosController {
     }
 
     @PostMapping
+    @Transactional
     public ResponseEntity<TopicoDto> cadastrar(@RequestBody @Valid TopicoForm form, UriComponentsBuilder uriBuilder){
         Topico topico = form.converter(cursoRepository);
         topicoRepository.save(topico);
@@ -62,6 +63,14 @@ public class TopicosController {
             Topico topico = form.atualizar(id,topicoRepository);
 
             return ResponseEntity.ok(new TopicoDto(topico));
+    }
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    public ResponseEntity remover(@PathVariable Long id){
+        topicoRepository.deleteById(id);
+
+        return ResponseEntity.ok().build();
     }
 
 }
