43bfaf08a5de7bcd8de453dbc1b42d23e39f2d4e
raphapfeifer <rapha_pfeifer@yahoo.com.br>
2020-05-24 14:52:41 -0300

    Filtros de paginação

--- forum/src/main/java/br/com/alura/forum/repository/TopicoRepository.java
+++ forum/src/main/java/br/com/alura/forum/repository/TopicoRepository.java
@@ -1,6 +1,8 @@
 package br.com.alura.forum.repository;
 
 import br.com.alura.forum.modelo.Topico;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -10,7 +12,7 @@ import java.util.List;
 public interface TopicoRepository  extends JpaRepository<Topico,Long> {
 
     // exemplo de busca pela convenção do spring data
-    List<Topico> findByCursoNome(String nomeCurso);
+    Page<Topico> findByCursoNome(String nomeCurso, Pageable paginacao);
 
     //exemplo sem convenção
     @Query("SELECT t FROM Topico t WHERE t.curso.nome =:nomeCurso")
