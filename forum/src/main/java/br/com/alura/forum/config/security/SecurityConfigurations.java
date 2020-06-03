ed5abd7fdb3f4058ed96a4818242686b111449b6
raphapfeifer <rapha_pfeifer@yahoo.com.br>
2020-06-02 21:20:30 -0300

    Habilitando Spring Security

---
+++ forum/src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
@@ -0,0 +1,14 @@
+package br.com.alura.forum.config.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
+
+
+
+}
