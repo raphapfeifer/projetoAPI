96f1c4ef68353d124f16489722473ea29a46d522
raphapfeifer <rapha_pfeifer@yahoo.com.br>
2020-06-07 21:56:54 -0300

    Autenticação de usuário

--- forum/src/main/resources/data.sql
+++ forum/src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '123456');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$tHhVfKjfOVlh44UifRhRRuL8xRsW802Ryh70HeCGNrqwi61fjjAt.');
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
