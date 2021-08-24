d19562ffbc787dd33c64ca6e624f12bccef1c148
raphapfeifer <rapha_pfeifer@yahoo.com.br>
2021-08-23 23:38:44 -0300

    Mais um commit de teste

--- forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -43,7 +43,7 @@ public class TopicosController {
     public Page<TopicoDto> lista(@RequestParam(required = false) String nomeCurso,
                                 @PageableDefault(sort = "id", direction = Sort.Direction.DESC) Pageable paginacao) {
 
-        // alterei o comentário
+        // alterei o comentário mais essa informação
         if (nomeCurso == null) {
             Page<Topico> topicos = topicoRepository.findAll(paginacao);
             return TopicoDto.converter(topicos);
